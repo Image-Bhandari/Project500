d2ecd6c5052db760930389c0e7ec429590b413a4
Image-Bhandari <50731001+Image-Bhandari@users.noreply.github.com>
2021-06-02 21:37:41 +0545

    new files

---
+++ public_html/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="/images/logo/logo.png" alt="Local Partners Pty Ltd">
+        </div>
+
+        <div class="login">
+            <a href="login.html">Login</a>
+        </div>
+        
+    </nav>
+
+    <main>
+        <section class="property-listing">
+            <section class="property">
+                <div class="details">
+                    <span>Property Name</span>
+                    <span>24-28 First Avenue Blacktown NSW 2148</span>
+                    <span>Details: 3 Bedroom Unit with 2 Bathroom</span>
+                    <span>Price: $1400</span>
+                </div>
+                
+                <div class="booking-options">
+                    <a href="book.html">Book Property</a>
+                    <a href="enquire">Enquire</a>
+                </div>
+            </section>
+
+            <section class="property">
+                <div class="details">
+                    <span>Property Name</span>
+                    <span>24-28 First Avenue Blacktown NSW 2148</span>
+                    <span>Details: 3 Bedroom Unit with 2 Bathroom</span>
+                    <span>Price: $1400</span>
+                </div>
+                
+                <div class="booking-options">
+                    <a href="book.html">Book Property</a>
+                    <a href="enquire">Enquire</a>
+                </div>
+            </section>
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
