a5185492b330205e0ad08a5efc9e5720751791b5
Image-Bhandari <50731001+Image-Bhandari@users.noreply.github.com>
2021-06-03 16:32:34 +0545

    updated sql file please add this

--- loyalpartners.sql
+++ loyalpartners.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2021 at 08:02 PM
+-- Generation Time: Jun 03, 2021 at 12:45 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `loyalpartners`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `properties`
+--
+
+CREATE TABLE `properties` (
+  `prop_id` int(255) NOT NULL,
+  `prop_name` varchar(255) NOT NULL,
+  `prop_addr` varchar(255) NOT NULL,
+  `prop_det` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `properties`
+--
+
+INSERT INTO `properties` (`prop_id`, `prop_name`, `prop_addr`, `prop_det`, `price`) VALUES
+(1, 'Mama Ghar', 'Bashundhara', '3 living , 1 cooking, 1 throwing rooms', '$550-50 = 500'),
+(2, 'Arru-Bari Ghar', 'Arru-bari', '4 floor but no room sorry', '$400');
 
 -- --------------------------------------------------------
 
@@ -44,11 +66,18 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `username`, `password`, `email`, `role`) VALUES
 (1, 'ram', 'ram', 'ram', 'ram', 'ram@ram.com', 'user'),
 (2, 'staff', 'staff', 'staff', 'staff', 'staff@staff.com', 'staff'),
-(3, 'admin', 'admin', 'admin', 'admin', 'admin@admin.com', 'admin');
+(3, 'admin', 'admin', 'admin', 'admin', 'admin@admin.com', 'admin'),
+(4, 'user', 'user', 'user', 'user', 'asd@sda.fdf', 'user');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `properties`
+--
+ALTER TABLE `properties`
+  ADD PRIMARY KEY (`prop_id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +90,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `properties`
+--
+ALTER TABLE `properties`
+  MODIFY `prop_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
