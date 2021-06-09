2a82d09a0353c169134619d5fa0b774c3e5ff004
surag04 <47417243+surag4@users.noreply.github.com>
2021-06-09 15:47:31 +1000

    admin side chat update

--- templates/admin/chatTemplate.php
+++ templates/admin/chatTemplate.php
@@ -1,44 +1,52 @@
-<div name="1">
-<h3> Chat You Handled </h3>
-<?php
-foreach ($handledname as $value) {
-    echo '<li><a href="index.php?page=chat&chat='. $value.'">'.$value.'</a></li><br><br>';
-}
-?>
-</div>
-<div name="2">
-<div name="2.1">
-<?php
-$client="";
-foreach ($message as $mes) {
-    if($mes['message_by']==$_SESSION['username']){
-        echo "You (".$_SESSION['username'].") : ". $mes['message'].'<br>';
-    }
-    else{
-        echo "Client (".$mes['message_by'].") : ". $mes['message'].'<br>';
-        $client=$mes['message_by'];
-        
-    }
-}
-?>
-</div>
-<br>
-<br>
-<br>
-<div name="2.2">
-<form method="post" action="index.php?page=chat">
-<input type="text" name="message">
-<input type="hidden" name="message_to" value=<?php if($client!=""){echo $client;} else echo " ";?>>
-<input type="hidden" name="message_by" value=<?php echo $_SESSION['username'];?>>
-<input type="submit" name="send" value="Send">
-</form>
-</div>
-</div>
-<div name ="3">
-<h3>Requests:</h3>
-<?php
-foreach ($unhandledname as $value) {
-    echo '<li><a href="index.php?page=chat&handle='. $value.'">'.$value.'</a></li><br><br>';
-}
-?>
-</div>
+<article class="chattemplate">
+
+    <div class="handled">
+        <h3> Your Clients </h3>
+        <?php
+        foreach ($handledname as $value) {
+            echo '<li><a class="msg" href="index.php?page=chat&chat='. $value.'">'.$value.'</a></li><br><br>';
+        }
+        ?>
+    </div>
+
+    <div class="chatscreen">
+        <div class="messages">
+            <?php
+            $client="";
+            foreach ($message as $mes) {
+                if($mes['message_by']==$_SESSION['username']){
+                    echo "<p class='messageto'> (".$_SESSION['username'].") : ". $mes['message'].'</p>';
+                }
+                else{
+                    echo "<p class='messageby'> (".$mes['message_by'].") : ". $mes['message'].'</p>';
+                    $client=$mes['message_by'];
+                    
+                }
+            }
+            ?>
+        </div>
+
+        <br>
+        <br>
+        <br>
+
+        <div class="messagebox">
+            <form method="post" action="index.php?page=chat">
+                <input type="text" name="message" required>
+                <input type="hidden" name="message_to" value=<?php if($client!=""){echo $client;} else echo " ";?>>
+                <input type="hidden" name="message_by" value=<?php echo $_SESSION['username'];?>>
+                <input type="submit" name="send" value="Send">
+            </form>
+        </div>
+    </div>
+
+    <div class ="requests">
+        <h3>New Client Requests</h3>
+        <?php
+            foreach ($unhandledname as $value) {
+                echo '<li><a class="msg" href="index.php?page=chat&handle='. $value.'">'.$value.'</a></li><br><br>';
+        }
+        ?>
+    </div>
+       
+</article> 
