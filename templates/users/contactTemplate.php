fbe628e4145aaab13fe988118dab66c490ed7c66
Surag Pandit <suragpandit@gmail.com>
2021-08-26 21:10:13 +1000

    major change in nav

---
+++ templates/users/contactTemplate.php
@@ -0,0 +1,6 @@
+<section>
+    <!-- IIBIT, 841 George St, Sydney NSW 2000 -->
+    <div class="map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.242623420052!2d151.2006673152322!3d-33.88340472724844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12b8cae750057b%3A0xdcaeace92b3f87fe!2sIIBIT!5e0!3m2!1sen!2sau!4v1629960128750!5m2!1sen!2sau" width="100%" height="300px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+</section>
