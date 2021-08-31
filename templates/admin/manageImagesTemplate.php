0868087a9623bd8cd0b102b74092dee412a4081b
Surag Pandit <suragpandit@gmail.com>
2021-08-31 18:11:02 +1000

    list images in grid view

--- templates/admin/manageImagesTemplate.php
+++ templates/admin/manageImagesTemplate.php
@@ -1,12 +1,32 @@
-<section>
-<table>
-<tr><th>Photos</th><th>Action</th></tr>
-<?php
-$directory = "../images/".$pid;
-$images = glob($directory . "/*");
-foreach($images as $chitra){
-echo '<tr><td><img src="'.$chitra.'"></td><td><a href="">Delete</a></td></tr>';
-}
-?>
-</table>
-</section>
+<article>
+
+    <div class="manageimg">
+        <div class="viewimg">Photos</div>
+        <div class="viewtitle">Picture Title</div>
+        <div class="viewact">Action</div>
+
+        <?php
+            $directory = "../images/".$pid;
+            $images = glob($directory . "/*");
+            foreach($images as $chitra){ 
+        ?>
+        <div class="photo-holdr">
+            <img src="<?php echo $chitra?>">                
+        </div>
+                
+        <div class="photo-title">
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, itaque!</span>
+            <div onclick="enable();" class="editbutton"><h5>Edit <i class="fas fa-pen"></i></h5></div>
+        </div>
+            
+        <div class="photo-actions">
+            <a href="">Update</a>
+            <a href="">Delete</a>
+        </div>
+        
+           
+            <?php 
+            //echo '<tr><td><img src="'.$chitra.'"></td><td></td></tr>';
+            }
+        ?>
+</article>
