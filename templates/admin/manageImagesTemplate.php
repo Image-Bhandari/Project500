688aafc984df78fb1314b796f20cfbfa121dc270
Surag Pandit <suragpandit@gmail.com>
2021-08-31 23:40:00 +1000

    changes in manage photos in admin

--- templates/admin/manageImagesTemplate.php
+++ templates/admin/manageImagesTemplate.php
@@ -2,7 +2,6 @@
 
     <div class="manageimg">
         <div class="viewimg">Photos</div>
-        <div class="viewtitle">Picture Title</div>
         <div class="viewact">Action</div>
 
         <?php
@@ -13,20 +12,13 @@
         <div class="photo-holdr">
             <img src="<?php echo $chitra?>">                
         </div>
-                
-        <div class="photo-title">
-            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, itaque!</span>
-            <div onclick="enable();" class="editbutton"><h5>Edit <i class="fas fa-pen"></i></h5></div>
-        </div>
             
         <div class="photo-actions">
-            <a href="">Update</a>
             <a href="">Delete</a>
         </div>
         
            
             <?php 
             //echo '<tr><td><img src="'.$chitra.'"></td><td></td></tr>';
-            }
-        ?>
+            } ?>
 </article>
