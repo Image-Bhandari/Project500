7eeb426477acbc02bd0f0c031e3670c5d291350f
Surag Pandit <suragpandit@gmail.com>
2021-09-16 02:17:51 +1000

    error fixed

--- pages/admin/notif.php
+++ pages/admin/notif.php
@@ -2,7 +2,7 @@
 	$title = "Notifications";
 	if(isset($_SESSION['login']) && $_SESSION['login'] == 1 && ($_SESSION['role']  == 'staff' || $_SESSION['role'] == 'admin')){
 		$conn = new DatabaseTable('notification');
-		$data=$conn->orderby('notif_id');
+		$data=$conn->orderOn('notif_id');
 
 		if(isset($_GET['did'])){
 			$del_status = $conn->delete("notif_id",$_GET['did']);
