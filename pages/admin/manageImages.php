ad0b3559d217608b3968fa9af357f14f9d8bb519
Surag Pandit <suragpandit@gmail.com>
2021-09-15 21:29:02 +1000

    error fixed
    If the image directory is deleted after adding a new property then you go to manage image to add a new image. It wouldn't let you upload because the directory doesnt exist and was throwing some errors.
    It has now been fixed.

--- pages/admin/manageImages.php
+++ pages/admin/manageImages.php
@@ -9,29 +9,32 @@ if(isset($_SESSION['login']) && $_SESSION['login'] == 1 && ($_SESSION['role']  =
 		}
 
 		if(isset($_GET['dpid'])){
-		if(unlink('../images/'.$pid.'/'.$_GET['dpid'].'.jpg')){
-			header('location:index.php?page=manageImages&pid='.$pid);
-		}
-		else if(unlink('../images/'.$pid.'/'.$_GET['dpid'].'.jpeg')){
-			header('location:index.php?page=manageImages&pid='.$pid);
-		}
-		else{echo 'Sorry Server Error.';}
+			if(unlink('../images/'.$pid.'/'.$_GET['dpid'].'.jpg')){
+				header('location:index.php?page=manageImages&pid='.$pid);
+			}
+			else if(unlink('../images/'.$pid.'/'.$_GET['dpid'].'.jpeg')){
+				header('location:index.php?page=manageImages&pid='.$pid);
+			}
+			else{echo 'Sorry Server Error.';}
 		}
 
 		if(isset($_POST['addPic'])){
-				$target_dir = "../images/".$_POST['pid']."/";
-				$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-				$uploadOk = 1;
-				$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-				if($check !== false) {
-				echo "File is an image - " . $check["mime"] . ".";
-				$uploadOk = 1;
-				} else {
-				echo "File is not an image.";
-				$uploadOk = 0;}
+			if( is_dir('../images/'.$_POST['pid']) === false ) {
+				mkdir('../images/'.$_POST['pid']);
+			}
+			$target_dir = "../images/".$_POST['pid']."/";
+			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+			$uploadOk = 1;
+			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if($check !== false) {
+			echo "File is an image - " . $check["mime"] . ".";
+			$uploadOk = 1;
+			} else {
+			echo "File is not an image.";
+			$uploadOk = 0;}
 
-				if($imageFileType != "jpg" && $imageFileType != "jpeg") {
+			if($imageFileType != "jpg" && $imageFileType != "jpeg") {
 		echo "Sorry, only JPG, JPEG files are allowed.";
 		$uploadOk = 0;
 		}
@@ -48,14 +51,16 @@ if(isset($_SESSION['login']) && $_SESSION['login'] == 1 && ($_SESSION['role']  =
 			foreach($images as $chitra){
 				array_push($lastVal,(int)substr($chitra,-5,-4));
 			}
-			$newfilename = (max($lastVal)+1);
-		if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $newfilename.'.jpg')){
-			header('Location:index.php?page=manageImages&pid='.$_POST['pid']);
-		}
-		else{
-			echo "Failed to do the operation. Please Try Again.";
-		}
-
+			if ($lastVal==null) {
+				$newfilename=1;
+			}else{
+			$newfilename = (max($lastVal)+1);}
+			if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $newfilename.'.jpg')){
+				header('Location:index.php?page=manageImages&pid='.$_POST['pid']);
+			}
+			else{
+				echo "Failed to do the operation. Please Try Again.";
+			}
 		}
 
 		}
